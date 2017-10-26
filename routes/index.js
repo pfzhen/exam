60b4ee3de385927aa88fe64460c74f40f38066c3
asdf <599348179@qq.com>
2017-10-26 15:11:10 +0800

    周考

--- routes/index.js
+++ routes/index.js
@@ -2,31 +2,14 @@ var express = require('express');
 var router = express.Router();
 var fs =require('fs')
 router.post('/', function(req, res, next) {
+  fs.readFile('public/jgr.txt','utf-8', function (err,data) {
+    console.log(data)
+  })
   res.header('Access-Control-Allow-Origin',"*")
-  var temp=[]
-  if(req.body.abc==1){
-    temp=[1,2,3]
-  }else{
-    temp=[4,5,6]
-  }
-//switch (req.body.abc){
-//  case "1":
-//    temp="nanzhuang"
-//        break;
-//  case "2":
-//        temp="nvzhuang"
-//        break;
-//}
+  var temp="123";
+  res.send({"name":temp})
 
-  res.send({ name: temp });
 });
-
-
-
-
-
-/* GET home page. */
-
 
 module.exports = router;
 
